8562552f90cf13b613aa3c56f8f84100ab6e0a3e
Bernardo Flores <breyflores13@yahoo.com>
2021-12-17 00:33:09 -0600

    edit

--- behavior/interactive.js
+++ behavior/interactive.js
@@ -1,10 +1,1 @@
 // interactive.js to control behavior between endpoints
-
-function show_image(src,width,height,alt) {
-	var img = document.createElement("img");
-	img.src = src;
-	img.width = width;
-	img.height = height;
-	img.alt = alt;
-	document.body.appendChild(img);
-}
