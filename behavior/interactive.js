7c5bf9320bbd1942d2ecef9515cabdf3309039a4
Bernardo Flores <breyflores13@yahoo.com>
2021-12-16 18:27:44 -0600

    Edit About ME

--- behavior/interactive.js
+++ behavior/interactive.js
@@ -1,1 +1,29 @@
 // interactive.js to control behavior between endpoints
+var image = new Array("images/image1.jpg", "images/image2.jpg", "images/image3.gif");
+var count = 0;
+var length = image.length - 1;
+ 
+ 
+function imageSlider(index){
+ 
+	count = count + index;
+ 
+	if(count > length){
+		count = 0;
+	}
+ 
+	if(count < 0){
+		count = length;
+	}
+ 
+	document.slideshow.src = image[count];
+ 
+	return false;
+}
+ 
+ 
+autoSlideShow();
+ 
+function autoSlideShow(){
+	setInterval("imageSlider(1)", 4000);
+}
